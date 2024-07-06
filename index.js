53533bd2a20ae1bd1b994c28e76eb3e1914a758c
kevin <kipronok234@gmail.com>
2024-07-06 11:27:09 +0300

    dom modifying elements

--- index.js
+++ index.js
@@ -1,1 +1,10 @@
 // Write your code here!
+const mainElement = document.querySelector('main#main')
+if(mainElement) {
+    mainElement.remove();
+}
+const newHeader = document.createElement('h1')
+newHeader.id ='victory'
+newHeader.innerHTML = 'Kirk Thompson is the champion'
+
+document.body.appendChild(newHeader);
